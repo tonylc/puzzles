6aee7ed34e88e1997490e4141b37ec86004fa9e3
Tony <tonylc@gmail.com>
2014-01-23 00:23:26 -0800

    add shitty combo

--- app/models/combination.rb
+++ app/models/combination.rb
@@ -10,4 +10,42 @@ class Combination
   end
 end
 
-#Combination.combinations('ABCDE')
+Combination.combinations('12345')
+
+
+
+# shittier way...
+@debug = false
+@arr = []
+
+def print_subset(arr)
+  num_sets = arr.length
+  for i in 0..num_sets
+    print_all_combinations(arr.slice(0, i), arr.slice(i, num_sets-i))
+    print_subset(arr.slice(i, num_sets-i)) if i != 0
+  end
+end
+
+# print selected_set and choose one from left over set
+# need to call this again w/ left over sets
+def print_all_combinations(selected_set, left_over_set)
+debug("first #{selected_set} - left over #{left_over_set}")
+  left_over_set.each do |left_over|
+    save(selected_set + [left_over])
+  end
+end
+
+def save(current_set)
+  debug("**** #{current_set}")
+  current_arr_index = current_set.join("").to_i
+  @arr[current_arr_index] = current_arr_index
+end
+
+def debug(message)
+  if @debug
+    p "****** #{message}"
+  end
+end
+
+print_subset([1,2,3,4,5])
+p "***** #{@arr.compact.inspect}"
